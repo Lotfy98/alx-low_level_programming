569e1a91722cd7ff4f67f8b6f457d8d934412d57
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-08-10 14:22:08 -0700

    Write a C program that prints followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - ENTRY POINT
+ *
+ *Description: Write a C program that prints exactly
+ *
+ *Return: 1 not success
+ */
+int main(void)
+{
+char x[]=" and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+write(1, x, 60);
+return (1);
+}
