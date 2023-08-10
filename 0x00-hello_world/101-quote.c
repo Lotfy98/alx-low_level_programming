d1cc31aad57c48bc09d37b57bd670ebcb4324224
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-08-10 14:23:14 -0700

    Write a C program that prints followed by a new line, to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-char x[]=" and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+char x[] = " and that piece of art is useful\" - Dora Korpar, 2015-10-19"
 write(1, x, 60);
 return (1);
 }
