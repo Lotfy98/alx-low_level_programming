28b07adf16cb23ae74038a4f02ced40c068521b0
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-08-11 08:53:40 -0700

    Write a program that prints the alphabet in lowercase, followed by a new line

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 /**
  *main -ENTRY POINT
  *
- *Description: a program that prints the alphabet in lowercase, followed by a new line.
+ *Description: a program that prints the alphabet in lowercase with new line.
  *
  *Return: 0 to success
  */
