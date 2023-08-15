70594fffa25d7b35b2d9dc0c2eb07f535d0bc5ae
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-08-16 00:22:58 +0300

    m

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_abs - function to print absolute of integer
+ *
+ *@n: integer 
+ *
+ *
+ *Return: 0 to success
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (0);
+}
