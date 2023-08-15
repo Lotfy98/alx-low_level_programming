280d48fc885f455c8cbf939e14209ae6fec549f5
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-08-15 22:58:17 +0300

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 -function that prints 10 times the alphabet in lowercase
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int x, r;
+
+	for (r = 0; r <= 9; r++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		putchar('\n');
+	}
+}
