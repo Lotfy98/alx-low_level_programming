c79db5b8d8c1a46a7732363701d9af0a7cef2e00
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-08-16 00:27:55 +0300

    m

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,14 +5,13 @@
  *@n: integer
  *
  *
- *Return: 0 to success
+ *Return: to success
  */
 int _abs(int n)
 {
-	if (n < 0)
+	if (n <= 0)
 	{
-		n *= (-1);
+		n = n * (-1);
 		return (n);
 	}
-	return (0);
 }
