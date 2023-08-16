affb9991549ed1b44afcec5f5e43ba39f4a8bc08
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-08-17 02:10:43 +0300

    a

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,10 +2,9 @@
 /**
  * main - tests function that prints if integer is positive or negative
  *
- *Description: -ve or +ve integer determine
- *
  * Return: 0 to success
  */
+
 int main(void)
 {
 	int i;
@@ -13,5 +12,4 @@ int main(void)
 	i = 0;
 	positive_or_negative(i);
 
-	return (0);
 }
