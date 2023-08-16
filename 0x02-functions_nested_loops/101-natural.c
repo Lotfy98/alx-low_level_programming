0ded9207fb4ebb978d80af4080c0a22bbc3402a2
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-08-16 03:01:32 +0300

    m

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <main.h>
+/**
+ *main - Entry point
+ *
+ *Descriuption: sum of 3 and 5 multiples below 1024
+ *
+ *Return: 0 to success
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x < 1024; x++)
+	{
+		if ((x % 3 == 0) || (x % 5  == 0))
+		{
+			y += x;
+		}
+	}
+	printf("%d\n", y);
+	return (0);
+}
