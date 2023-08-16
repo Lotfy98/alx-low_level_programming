d39024ddbfb7c8ed0acf1b7d891291521707f623
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-08-16 03:30:45 +0300

    m

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *main - Entry point
+ *
+ *Description:
+ * 
+ *Return: 0 to success
+ */
+int main(void)
+{
+	unsigned long f1 = 0, f2 = 1, x;
+	float totalSum;
+
+	while (1)
+	{
+		x = f1 + f2;
+		if (x > 4000000)
+		{
+			break;
+		}
+		if ((x % 2) == 0)
+		{
+			totalSum += x;
+		}
+		f1 = f2;
+		f2 = x;
+	}
+	printf("%.0f\n", totalSum);
+	retrun (0);
+}
