ed07ba89c3543ac430f91f9d6192b46be6faa67b
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-08-17 23:14:04 +0300

    m

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,17 +10,16 @@ void print_diagonal(int n)
 	while (n <= 0)
 	{
 		_putchar('\n');
-	}		
-	while ( n > 0)
+	}
+	while (n > 0)
 	{
 		for (x = 0; x < n; x++)
 		{
 			for (y = 0; y < n; y++)
 				_putchar(' ');
-				
+
 			_putchar(92);
 			_putchar('\n');
 		}
-		n++;
 	}
 }
