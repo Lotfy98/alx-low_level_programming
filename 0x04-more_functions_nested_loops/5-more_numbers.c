b3c005274addeb7b4d6f6d6e34b7d0a874144704
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-08-17 18:23:16 +0300

    m

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,13 +5,19 @@
  */
 void more_numbers(void)
 {
-	int x, y;
+	int x, y, z;
 
 	for (x = 0; x < 10; x++)
 	{
 		for (y = 0; y <= 14; y++)
 		{
-			_putchar(y + 48);
+			y = z;
+			if (y > 9)
+			{
+				_putchar(1 + 48);
+				z = y % 10;
+			}
+			_putchar(z + 48);
 		}
 		_putchar('\n');
 	}
