5e3faad1a31939a188f0d323779bc4a8c76b8c9c
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-08-18 01:01:57 +0300

    m

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *print_triangle - print triangle
+ *@size: size of triangle
+ */
+void print_triangle(int size)
+{
+	int x, y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (y = 1; y <= size; y++)
+			{
+				if ((x + y) <= size)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
