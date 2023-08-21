136fb9dfa7f828f71d7f25dd02f6e7b35eeea0bf
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-08-22 02:22:15 +0300

    m

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _strlen - a function that returns the length of a string
+ * @s: character
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length += 1;
+		s++;
+	}
+	return (length);
+}
+
+
+/**
+ * puts2 - prints every other character of string
+ * starting with 1st char
+ * @str: string
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (i <= _strlen(str))
+	{
+		_putchar(s[i]);
+		i += 2;
+	}
+}
