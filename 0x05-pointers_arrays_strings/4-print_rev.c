77c6bdc38fc534b95718f003d35d7fba5e68f352
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-08-22 01:02:30 +0300

    m

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,21 @@
 #include "main.h"
 /**
+ *_strlen - a function that returns the length of a string
+ * @s: character
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length += 1;
+		s++;
+	}
+	return (length);
+}
+/**
  * print_rev - prints a string, in reverse, followed by a new line
  * @s: character
  */
