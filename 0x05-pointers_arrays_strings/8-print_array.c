52766f989e066005038147037f6904378087451c
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-08-22 03:49:40 +0300

    m

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_array - function that prints n elements of an array of integers
+ *@a: integer
+ *@n: integer
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (i != (n - 1))
+		{
+			printf("%i, ", a[i]);
+		}
+		else
+			printf("%i, ", a[i]);
+	}
+	printf("\n");
+}
