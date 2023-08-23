e666dc8ba9e0cbb2822713e9ffb882a4abf6f5be
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-08-23 22:08:36 +0300

    m

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *string_toupper - changes lowercase of string to uppercase
+ *@str: string
+ *Return: uppercase string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] -= 32;
+		}
+	}
+	return (str);
+}
