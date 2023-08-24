2eb5dd243c2487705e3911e90fe46e951f756ff5
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-08-24 23:24:34 +0300

    m

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,10 +9,8 @@ int _strlen(char *s)
 	int length = 0;
 
 	while (*s != '\0')
-	{
 		length += 1;
 		s++;
-	}
 	return (length);
 }
 /**
@@ -25,53 +23,43 @@ int _strlen(char *s)
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = _strlen(n1);
-	int e = _strlen(n2);
-	int k, f, s, d = 0;
+	int i = _strlen(n1), int e = _strlen(n2), int k, f, s, d = 0;
 
 	if (i > e)
-	{
 		l = i;
-	}
 	else
-	{
 		l = e;
-	}
+
 	if (l + 1 > size_r)
-	{
 		return (0);
-	}
 	r[l] = '\0';
 	for (k = l - 1; k >= 0; k--)
 	{
 		i--;
 		e--;
 		if (i >= 0)
-		{
 			f = n1[i] - 48;
-		}
 		else
-		{
 			f = 0;
-		}
+
 		if (e >= 0)
-		{
 			s = n2[e] - 48;
-		}
 		else
-		{
 			s = 0;
-		}
+
 		r[k] = (f + s + d) % 10 + 48;
 		d = (f + s + d) / 10;
 	}
 	if (d == 1)
 	{
 		r[l + 1] = '\0';
+
 		if (l + 2 > size_r)
 			return (0);
+
 		while (l--)
 			r[l + 1] = r[l];
+
 		r[0] = d + 48;
 	}
 	return (r);
