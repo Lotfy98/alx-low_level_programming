21ba27e449c128e3894a9f9564ab70b234a8fa05
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-08-24 04:31:43 +0300

    m

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *isLower - determines lowercase
+ *@c: character
+ *Return: 1 if true, 0 if false
+ */
+int isLower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
+
+/**
+ *isDelimeter - determines if delimeter
+ *@c: character
+ *Return: 1 if true, 0 if false
+ */
+int isDelimeter(char c)
+{
+	int i;
+	char delimeter[] = " \t\n,.!?\"(){}";
+
+	for (i = 0; i < 12; i++)
+	{
+		if (c == delimeter[i])
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+/**
+ *cap_string - capitalize all words of string
+ *@s: string
+ *Return: capitalized string
+ */
+char *cap_string(char *s)
+{
+	char *ptr = s;
+	int delimet = 1;
+
+	while (*s)
+	{
+		if (isDelimeter(*s))
+		{
+			delimit = 1;
+		}
+		else if (isLower(*s) && delimit)
+		{
+			*s -= 32;
+			delimit = 0;
+		}
+		else
+		{
+			delimit = 0;
+		}
+		s++;
+	}
+	return (ptr);
+}
