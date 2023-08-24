886805fc657d6e660d2fcb9925cc283e3bbb61f8
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-08-24 12:51:17 +0300

    m

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * leet - encodes string to 1337
+ *@s:string
+ *Return: encoded string
+ */
+char *leet(char *s)
+{
+	int i;
+	char x[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char y[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	
+	while (*s)
+	{
+		for (i = 0; i <= 9 ; i++)
+		{
+			if (*s == x[i] || *s == x[i] - 32)
+			{
+				*s = y[i];
+			}
+		}
+		s++;
+	}
+	return (s);
+}
+
+		
