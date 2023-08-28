5f213863c657b7d26737c91c5266a7dfbba24f11
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-08-28 20:41:19 +0300

    m

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_memset - fills memory with constant byte
+ *@s:string
+ *@b: character
+ *@n: max bytes
+ *Return: string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	size_t x = 0;
+
+	while (x < n)
+	{
+		((char *) s)[x] = b;
+		x++;
+	}
+}
