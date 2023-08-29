5291511328c8820a45d030351e63ec6be23fd087
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-08-29 03:31:28 +0300

    m

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *_strstr - locates a substring
+ *@haystack: string
+ *@needle: character
+ *Return: pointer to haystack or  '\0' if not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+	while (*haystack != 0)
+	{
+		char *n1 = haystack;
+		char *n2 = needle;
+
+		while (*n1 == *n2 && *n2 != '\0')
+		{
+			n1++;
+			n2++;
+		}
+		if (*n2 == '\0')
+			return(haystack);
+
+		haystack++;
+	}
+	return (NULL);
+}
