bdc504485de11553c3335d535cd006e0df18de38
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-08-29 03:34:43 +0300

    m

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,17 +10,16 @@ char *_strstr(char *haystack, char *needle)
 
 	while (*haystack != 0)
 	{
-		char *n1 = haystack;
-		char *n2 = needle;
-
+		char *n1 = haystack, *n2 = needle;
 		while (*n1 == *n2 && *n2 != '\0')
 		{
 			n1++;
 			n2++;
 		}
 		if (*n2 == '\0')
+		{
 			return (haystack);
-
+		}
 		haystack++;
 	}
 	return (NULL);
