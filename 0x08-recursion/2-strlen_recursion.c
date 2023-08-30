0b488bbb44b6d989a1547b7dc115a6f550328abe
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-08-30 12:43:45 +0300

    m

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_strlen_recursion - returns the length of string
+ *@s: string
+ *Return: counter
+ */
+int _strlen_recursion(char *s)
+{
+	int counter = 0;
+
+	if (*s > '\0')
+	{
+		counter += _strlen_rev_recursion(s + 1) + 1;
+	}
+	return (counter);
+}
