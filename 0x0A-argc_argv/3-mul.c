8b5943aeaafc5d6a89239ab2dfe2f51cb8296b1d
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-09-01 23:23:40 +0300

    m

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *@argc: int
+ *@argv: string
+ *Return: 0 to succes, error with 1
+ */
+int main(argc, char *argv[])
+{
+	int result;
+
+	if (argc !=  3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	result = atoi(argv[1]) * atoi(argv[2]);
+	printf("%i\n", result);
+	return (0);
+}
