779e692fcbfe8166f39c1d4554de78a204495c7d
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-09-01 22:17:25 +0300

    m

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *@argc: integer
+ *@argv: list
+ *Return: 0 to success
+ */
+int main(int agrc, char const *agrv __attribute__((unused)))
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
