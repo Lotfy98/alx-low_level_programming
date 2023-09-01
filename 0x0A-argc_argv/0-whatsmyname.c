431c4be5dbfd8b0e9e44c164bf633e9244c92520
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-09-01 22:18:48 +0300

    m

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  *@argv: list
  *Return: 0 to success
  */
-int main(int agrc, char const *agrv __attribute__((unused)))
+int main(int agrc __attribute__((unused)), char const *agrv)
 {
 	printf("%s\n", argv[0]);
 	return (0);
