e690ee6b93c5ee362001684ec88f5d747614f54d
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-09-02 00:28:32 +0300

    m

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - entry point
+ *@argc: int
+ *@argv: array
+ *Return: 0 to success
+ */
+int main(int argc, char *argv[])
+{
+	int result = 0;
+	char *x;
+
+	while (--argc)
+	{
+		x = argv[argc];
+		while (*x)
+		{
+			if (*x < '0' || *x > 9)
+			{
+				return (printf("Error\n"), 1);
+			}
+			x++;
+		}
+		result += atoi(agrv[argc]);
+	}
+	printf("%i\n", result);
+	return (0);
+}
