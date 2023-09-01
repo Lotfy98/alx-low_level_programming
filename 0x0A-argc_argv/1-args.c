b80122b327c661d1a879743c76f0040dc33815a0
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-09-01 22:56:01 +0300

    m

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *@argc: integer
+ *@argv: pointer to string
+ *Return: 0 to success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%i\n", argc);
+	return (0);
+}
