96859969ce74277f69766074a3cdfb327952d3e0
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-09-05 22:22:57 +0300

    m

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int i = 0;
 
-	array = malloc(size * 1);
+	array = malloc(size * sizeof(char));
 	if (array == NULL)
 		return (NULL);
 
