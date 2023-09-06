3811801cd365b44bfa3e3acda224f894c61e61b0
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-09-06 23:26:46 +0300

    m

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -28,7 +28,7 @@ int wordCounter(char *s)
 /**
  *strtow - spilts string to words
  *@str: string
- *@Return: array of words
+ *Return: array of words
  */
 char **strtow(char *str)
 {
@@ -37,52 +37,38 @@ char **strtow(char *str)
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
-
 	n = wordCounter(str);
-
 	if (n == 1)
 		return (NULL);
-
 	z = (char **)malloc(n * sizeof(char *));
-
 	if (z == NULL)
 		return (NULL);
-
 	z[n - 1] = NULL;
-
 	while (str[i])
 	{
 		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
 		{
 			for (e = 1; str[i + e] != ' ' && str[i + e]; e++)
 				;
-
 			e++;
 			z[wordCount] = (char *)malloc(e * sizeof(char));
 			e--;
-
 			if (z[wordCount] == NULL)
 			{
 				for (x = 0; x < wordCount; x++)
 					free(z[x]);
-
 				free(z[n - 1]);
 				free(z);
 				return (NULL);
 			}
-
 			for (y = 0; y < e; y++)
-			{
 				z[wordCount][y] = str[i + y];
-			}
-
 			z[wordCount][y] = '\0';
 			wordCount++;
 			i += e;
 		}
 		else
 			i++;
-
 	}
 	return (z);
 }
