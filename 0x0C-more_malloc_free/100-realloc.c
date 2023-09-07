92dbc7ba959742a8fd92efa46dd75a5b2e989ac9
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-09-07 22:24:26 +0300

    m

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -32,12 +32,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	void *newPtr;
 	unsigned int mv;
 
+	if (new_size == 0)
+		return (NULL);
 	if (ptr == NULL)
-	{
-		if (new_size == 0)
-			return (NULL);
 		return (malloc(new_size));
-	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
