e7718ba909d6ca29676d7fb8aa7724119a7d23fb
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-09-07 23:00:08 +0300

    m

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -29,7 +29,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *newPtr;
+	char *newPtr;
 	unsigned int mv;
 
 	if (ptr == NULL)
