c20508e861511986a116fc148ad6f523f0777c73
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-09-07 20:57:27 +0300

    mend
    m

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -33,7 +33,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	x = malloc(nmemb * sizeof(int);
+	x = malloc(nmemb * sizeof(int));
 
 	if (x == 0)
 		return (NULL);
