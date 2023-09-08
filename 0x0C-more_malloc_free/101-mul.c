f142a295484b4c0ab2a332f4092ec40b2ffbbcc0
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-09-08 23:16:16 +0300

    m

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <unistd.h>
 /**
  * _putchar - write char c to stdout
  * @c: char to print
@@ -9,20 +10,6 @@
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
-}
-/**
- *_puts -  function that prints a string, followed by a new line, to stdout
- *@str: charcter
- */
-void _puts(char *str)
-{
-	int i;
-
-	for (i = 0; *str != '\0'; str++)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
 }
 /**
  *_isdigit - function to check for digit through 0 to 9
@@ -39,22 +26,6 @@ int _isdigit(int c)
 	{
 		return (0);
 	}
-}
-/**
- *_strlen - a function that returns the length of a string
- *@s: character
- *Return: length of string
- */
-int _strlen(char *s)
-{
-	int length = 0;
-
-	while (*s != '\0')
-	{
-		length += 1;
-		s++;
-	}
-	return (length);
 }
 /**
  *_atoi - converts string to integer
@@ -84,72 +55,25 @@ int _atoi(char *s)
 	return (x * y);
 }
 /**
- *mult - multiplies two integers
- *@n1: integer
- *@n2: integer
- *@l1: length integer 1
- *@l2: length integer 2
- *Return: multiply result
- */
-int mult(int *n1, int *n2, int l1, int l2)
-{
-	int *z = malloc((l1 + l2) * sizeof(int));
-	int *result;
-	int i = 0, e = 0, c = 0, r = 0, s;
-
-	while (i < l1 + l2)
-	{
-		z[i] = 0;
-		i++;
-	}
-	while (i < l1)
-	{
-		while (e < l2)
-		{
-			z[i + e] += n1[i] * n2[e];
-			e++;
-		}
-	}
-	for (s = l1 + l2 - 1; s >= 0; s--)
-	{
-		z[s] += c;
-		c = z[s] / 10;
-		z[s] %= 10;
-	}
-	while (i < l1 + l2)
-	{
-		if (z[i] != 0)
-		{
-			r = i + 1;
-			break;
-		}
-		i++;
-	}
-	result = malloc(r * sizeof(int));
-
-	while (i < r)
-	{
-		result[i] = z[i];
-		i++;
-	}
-	free(z);
-	return (result);
-}
-/**
  *_print - prints integer
  *@n: integer
  *Return: 0
  */
 void _print(unsigned long int n)
 {
-	unsigned long int x = 1, y, z;
+	unsigned long int x = 0, y = 0, z;
 
-	for (y = 0; n  / x > 9; i++, x *= 10)
-		;
-	for (; x >= 1; n %= x, x /= 10)
+	while (n  / x > 9)
+	{
+		y++;
+		x *= 10;
+	}
+	while (x >= 1)
 	{
 		z = n / x;
 		_putchar('0' + z);
+		n %= x;
+		x /= 10;
 	}
 }
 /**
@@ -160,27 +84,34 @@ void _print(unsigned long int n)
  */
 int main(int argc, char*argv[])
 {
-	int l1, l2, *n1, *n2, *result;
-
 	if (argc != 3)
 	{
-		_puts("Error ");
-		exit(98);
-	}
-	if (!_isdigit(argv[1][0]) || !_isdigit(argv[2][0]))
-	{
-		_puts("Error ");
+		_print("Error\n");
 		exit(98);
 	}
 	l1 = _strlen(argv[1]);
+	while (i < l1)
+	{
+		if (!_isdigit(argv[1][i]))
+		{
+			_print("Error\n");
+			exit(98);
+		}
+		i++;
+	}
 	l2 = _strlen(argv[2]);
+	while (i < l2)
+	{
+		if (!_isdigit(argv[2][i]))
+		{
+			_print("Error\n");
+			exit(98);
+		}
+		i++;
+	}
 	n1 = _atoi(argv[1]);
 	n2 = _atoi(argv[2]);
-	result = mult(n1, n2, l1, l2);
-	_print( result[0]);
-	_putchar('\n');
-	free(n1);
-	free(n2);
-	free(result);
+	res = n1 * n2;
+	_print(res);
 	return (0);
 }
