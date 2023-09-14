8afb84f85853106e6f659ab5c712d6dfe698e6ae
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-09-14 18:12:07 +0300

    m

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -32,7 +32,7 @@ void print_all(const char * const format, ...)
 			}
 			case ('s'):
 			{
-				s = va_arg(args, char*);
+				s = va_arg(args, char *);
 				printf("%s", s ? s : "(nil)");
 				break;
 			}
