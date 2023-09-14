1c865d2c0db95b7b70acab926edd8c64d842596e
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-09-14 19:31:51 +0300

    m

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,42 +6,42 @@
  */
 void print_all(const char * const format, ...)
 {
-	int x, i = 0;
+	int x, i = 0, y = 0;
 	char c, *s;
 	const char syms[] = "cifs";
 	va_list args;
 
 	va_start(args, format);
-
-
 	while (format && format[i] != '\0')
 	{
-		int y = 0;
-
 		while (syms[y])
 		{
 			if (format[i] == syms[y] && x)
 			{
 				printf(", ");
 				break;
-			}
-			y++;
+			} y++;
 		}
 		switch (format[i])
 		{
 			case ('c'):
 				c = va_arg(args, int);
-				printf("%c", c), x = 1, break;
+				printf("%c", c);
+				x = 1;
+				break;
 			case ('i'):
-				printf("%d", va_arg(args, int)), x = 1, break;
+				printf("%d", va_arg(args, int));
+				x = 1;
+				break;
 			case ('f'):
-				printf("%f", va_arg(args, double)), x = 1, break;
+				printf("%f", va_arg(args, double));
+				x = 1;
+				break;
 			case ('s'):
 				s = va_arg(args, char *);
-				printf("%s", s ? s : "(nil)"), x = 1;
-		}
-		i++;
-	}
-	printf("\n");
+				printf("%s", s ? s : "(nil)");
+				x = 1;
+		} i++;
+	} printf("\n");
 	va_end(args);
 }
