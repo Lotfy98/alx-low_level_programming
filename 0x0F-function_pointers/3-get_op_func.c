71f43db5e3d5d6888569838f4ab6fd3662180903
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-09-14 05:45:58 +0300

    m

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,7 +18,7 @@ int (*get_op_func(char *s))(int, int)
 			{NULL, NULL}
 	};
 
-	for (i = 0; i < 5 i++)
+	for (i = 0; i < 5; i++)
 	{
 		if (s && s[0] == ops[i].op[0] && !s[1])
 			return (ops[i].f);
