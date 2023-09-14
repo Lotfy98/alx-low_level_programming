963c1276cba30bad7b9306bb418c6d2b47c4adb0
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-09-14 17:59:49 +0300

    m

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,46 @@
+#include "variadic_functions.h"
+/**
+ *print_all - prints ath based on strins format
+ *@format: string
+ *Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	char *s, *f;
+	va_list args;
+
+	va_start(args, format);
+	f = (char *)format;
+	while (*f != NULL)
+	{
+		switch (*f++)
+		{
+			case ('c'):
+			{
+				printf("%c", va_arg(args, int));
+				break;
+			}
+			case ('i' || 'd')
+			{
+				printf("%d", va_arg(args, int));
+				break;
+			}
+			case ('f')
+			{
+				printf("%f", va_arg(args, double));
+				break;
+			}
+			case ('s')
+			{
+				s = va_arg(args, char*);
+				printf("%s", s ? s : "(nil)");
+				break;
+			}
+		}
+		if ((*f) && (*f != ' '))
+			printf(", ");
+	}
+	printf("\n");
+	va_end(args);
+}
+
