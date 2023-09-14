4ffda4f00b9448ec7c65c354dd6985c25d566219
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-09-14 14:01:40 +0300

    m

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,15 +9,17 @@
 int main(int argc, char *argv[])
 {
 	char *z = (char *)main;
-	int i, x;
+	int x;
 
 	if (argc != 2)
-		printf("Error\n"),exit(1);
+		printf("Error\n"), exit(1);
 
 	x = atoi(argv[1]);
 	if (x < 0)
-		printf("Error\n"),exit(2);
+		printf("Error\n"), exit(2);
+
 	while (x--)
 		printf("%02hx%s", *z++, x ? " " : "\n");
+
 	return (0);
 }
