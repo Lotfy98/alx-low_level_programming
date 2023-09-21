b619f6c8a81bed25cbb57c1c595d3590c4e9df21
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-09-21 21:17:03 +0300

    m

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *print_list - prints list_l
+ *@h: pointer to list
+ *Return: num of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%i] %s\n", h->len, h->str);
+		h = h->next;
+		nodes++;
+	}
+	return nodes;
+}
