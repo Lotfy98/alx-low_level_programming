fb915eb9e4505b9793f4f69cfb3bbc13c2b09a37
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-09-26 19:30:17 +0300

    m

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,9 +9,6 @@ size_t print_listint_safe(const listint_t *head)
 	size_t nodes = 0;
 	long int diff;
 
-	if (head == NULL)
-		exit(98);
-
 	while (head)
 	{
 		diff = head - head->next;
@@ -25,5 +22,7 @@ size_t print_listint_safe(const listint_t *head)
 			break;
 		}
 	}
+	if (head == NULL)
+		exit(98);
 	return (nodes);
 }
