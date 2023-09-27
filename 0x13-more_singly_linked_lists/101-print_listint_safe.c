395b02a62cede52f0ec164cc403d292a9d0de54f
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-09-27 18:27:45 +0300

    m

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,38 +7,24 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t nodes = 0, i = 0;
-	long int diff;
-	const listint_t *arr[1024];
+	const listint_t **args = NULL;
 
 	while (head)
 	{
 		while (i < nodes)
 		{
-			if (arr[i] == head)
+			if (args[i] == head)
 			{
-				printf("-> [%p] %i\n", (void *)head, head->n);
-				return (nodes);
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free(args);
 			}
 			i++;
 		}
-		arr[nodes] = head;
 		nodes++;
-		diff = head - head->next;
-		printf("[%p] %i\n", (void *)head, head->n);
-		if (diff > 0 && head->next)
-		{
-			head = head->next;
-		}
-		else
-		{
-			if (head->next)
-			{
-				printf("-> [%p] %i\n", (void *)head->next, head->next->n);
-			}
-			else
-				exit(98);
-			break;
-		}
+		args = _realloc(args, num, head);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 	}
+	free(args);
 	return (nodes);
 }
