204c916f18b9d5f4c241f74bda1494b416ad045a
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-09-28 23:58:08 +0300

    m

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *get_endianness - returns endianness of sys
+ *Return: 0 if big, 1 if small
+ */
+int get_endianness(void)
+{
+	unsigned int x = 1;
+	char *c = (char*)&x;
+	return (int)*c;
+}
