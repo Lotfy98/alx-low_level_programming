7c842e039ef9ef3b514c0a727b10d7348deca723
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-10-02 23:49:42 +0300

    m

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,13 +12,13 @@ int append_text_to_file(const char *filename, char *text_content)
 	size_t len;
 
 	if (!filename)
-		rturn (-1);
+		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 	if (tex_content == NULL)
-		retunr (1);
+		return (1);
 
 	len = 0;
 	while (text_content[len] != '\0')
