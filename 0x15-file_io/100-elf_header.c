40edb03304677dd030f3988b51daee57abb03494
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-10-04 00:27:42 +0300

    m

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,53 +1,46 @@
 #include "main.h"
-#define BUFFER_SIZE 1024
-
-int main(int argc, char *argv[])
+/**
+ *main - entry point
+ *@argc: argument count
+ *@argv: argument value
+ *Return: 0 to success
+ */
+int main(int argc, char **argv)
 {
-int fd;
-ssize_t bytes_read;
-unsigned char e_ident[EI_NIDENT];
+	int fd;
+	Elf64_Ehdr ehdr;
 
-if (argc != 2)
-{ fprintf(stderr, "Usage: elf_header elf_filename\n");
-exit(98); }
-fd = open(argv[1], O_RDONLY);
-if (fd < 0)
-{ fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
-exit(98); }
-bytes_read = read(fd, &e_ident, EI_NIDENT);
-if (bytes_read != EI_NIDENT)
-{ fprintf(stderr, "Error: Can't read ELF header\n");
-close(fd);
-exit(98); }
-if (e_ident[EI_MAG0] != ELFMAG0 || e_ident[EI_MAG1] != ELFMAG1 ||
-e_ident[EI_MAG2] != ELFMAG2 || e_ident[EI_MAG3] != ELFMAG3)
-{ fprintf(stderr, "Error: Not an ELF file\n");
-close(fd);
-exit(98); }
-lseek(fd, 0, SEEK_SET);
-if (e_ident[EI_CLASS] == ELFCLASS32)
-{ Elf32_Ehdr header;
-bytes_read = read(fd, &header, sizeof(header));
-if (bytes_read != sizeof(header))
-{ fprintf(stderr, "Error: Can't read ELF header\n");
-close(fd);
-exit(98);
-}
-printf("Entry point address:               ");
-printf("0x%x\n", header.e_entry); }
-else if (e_ident[EI_CLASS] == ELFCLASS64)
-{ Elf64_Ehdr header;
-bytes_read = read(fd, &header, sizeof(header));
-if (bytes_read != sizeof(header))
-{ fprintf(stderr, "Error: Can't read ELF header\n");
-close(fd);
-exit(98); }
-printf("Entry point address:               ");
-printf("0x%lx\n", header.e_entry); }
-else
-{ fprintf(stderr, "Error: Unknown ELF class\n");
-close(fd);
-exit(98); }
-close(fd);
-return (0);
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+		return (98);
+	}
+	fd = open(argv[1], O_RONLY);
+	if (fd == -1)
+	{
+		perror(argv[1]);
+		return (98);
+	}
+	if (read(fd, &ehdr, sizeof(ehdr)) != sizeof(ehdr))
+	{
+		fprintf(stderr, "%s is not an ELF file\n", argv[1]);
+		return (98);
+	}
+	if (ehdr.e_ident[EI_MAG0] != ELFMAG0 || ehdr.e_ident[EI_MAG1] != ELFMAG1
+	|| ehdr.e_ident[EI_MAG2] != ELFMAG2 || ehdr.e_ident[EI_MAG3] != ELFMAG3)
+	{
+		fprintf(stderr, "%s is not an ELF file\n", argv[1]);
+		return (98);
+	}
+	printf("ELF Header:\n");
+	print_magic(ehdr.e_ident);
+	print_class(ehdr.e_ident);
+	print_data(ehdr.e_ident);
+	print_version(ehdr.e_ident);
+	print_osabi(ehdr.e_ident);
+	print_abiversion(ehdr.e_ident);
+	print_type(ehdr.e_type);
+	print_entry(ehdr.e_entry);
+	close(fd);
+	return (0);
 }
