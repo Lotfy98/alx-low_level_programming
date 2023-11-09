ab4c80e1ad1a5ceffbe88e4a708bb5995c188e1b
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-11-09 20:58:24 +0200

    m

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: pointer to head of list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	while (!head)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
