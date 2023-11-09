08aac94fb7ad0d151baef3cbcf0b43bfe08f152c
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-11-09 20:30:26 +0200

    m

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,14 +7,14 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *current;
-	size_t = 0;
+	size_t size = 0;
 
 	current = h;
 	while(current)
 	{
 		printf("%d\n", current->n);
 		current = current->next;
-		size ++;
+		size++;
 	}
 	return (size);
 }
