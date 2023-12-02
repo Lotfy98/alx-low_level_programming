a6385906215a62b1d88f24162aae5f4bd1a505c7
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-12-02 04:29:15 +0200

    m

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,31 +9,31 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
- hash_node_t *node;
- unsigned long int i;
- unsigned char comma_flag = 0;
+hash_node_t *node;
+unsigned long int i;
+unsigned char comma_flag = 0;
 
- if (ht == NULL)
-		return;
+if (ht == NULL)
+	return;
 
- printf("{");
- for (i = 0; i < ht->size; i++)
- {
-		if (ht->array[i] != NULL)
+printf("{");
+for (i = 0; i < ht->size; i++)
+{
+	if (ht->array[i] != NULL)
+	{
+		if (comma_flag == 1)
+			printf(", ");
+
+		node = ht->array[i];
+		while (node != NULL)
 		{
-			if (comma_flag == 1)
+			printf("'%s': '%s'", node->key, node->value);
+			node = node->next;
+			if (node != NULL)
 				printf(", ");
-
-			node = ht->array[i];
-			while (node != NULL)
-			{
-				printf("'%s': '%s'", node->key, node->value);
-				node = node->next;
-				if (node != NULL)
-					printf(", ");
-			}
-			comma_flag = 1;
 		}
- }
- printf("}\n");
+		comma_flag = 1;
+	}
+}
+printf("}\n");
 }
